6742b2720acaf0d292639383fe25c0653ecca0f0
Marcelo Nobre <marcnobremorais@gmail.com>
2026-03-13 19:16:59 -0300

    Adicionar funcao para interacao da entidade coluna no banco de dados

---
+++ src/repository/repositoryColumn.ts
@@ -0,0 +1,27 @@
+import { pool } from "../db/connection";
+
+export async function findBoardById(id: number) {
+  const result = await pool.query(
+    "SELECT id FROM quadro WHERE id=$1",
+    [id]
+  );
+
+  return result.rows[0];
+}
+
+export async function createColumn(nome: string, ordem: number, quadro_id: number) {
+
+  const query = `
+    INSERT INTO coluna (nome, ordem, quadro_id)
+    VALUES ($1,$2,$3)
+    RETURNING *
+  `;
+
+  const result = await pool.query(query, [
+    nome,
+    ordem,
+    quadro_id
+  ]);
+
+  return result.rows[0];
+}
