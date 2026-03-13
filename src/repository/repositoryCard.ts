d103e638de656ddba725207939f978191f32232e
Marcelo Nobre <marcnobremorais@gmail.com>
2026-03-13 19:16:44 -0300

    Adicionar funcao para interacao da entidade card no banco de dados

---
+++ src/repository/repositoryCard.ts
@@ -0,0 +1,73 @@
+import { pool } from "../db/connection";
+
+export async function findColumnById(id:number){
+
+  const result = await pool.query(
+    "SELECT id FROM coluna WHERE id=$1",
+    [id]
+  );
+
+  return result.rows[0];
+}
+
+export async function findUserById(id:number){
+
+  const result = await pool.query(
+    "SELECT id FROM usuario WHERE id=$1",
+    [id]
+  );
+
+  return result.rows[0];
+}
+
+export async function createCard(
+  titulo:string,
+  descricao:string,
+  coluna_id:number,
+  usuario_id:number
+){
+
+  const query = `
+  INSERT INTO card (titulo,descricao,coluna_id,usuario_id)
+  VALUES ($1,$2,$3,$4)
+  RETURNING *
+  `;
+
+  const result = await pool.query(query,[
+    titulo,
+    descricao,
+    coluna_id,
+    usuario_id
+  ]);
+
+  return result.rows[0];
+}
+
+export async function findCardById(id:number){
+
+  const result = await pool.query(
+    "SELECT coluna_id FROM card WHERE id=$1",
+    [id]
+  );
+
+  return result.rows[0];
+}
+
+export async function findBoardByColumn(columnId:number){
+
+  const result = await pool.query(
+    "SELECT quadro_id FROM coluna WHERE id=$1",
+    [columnId]
+  );
+
+  return result.rows[0];
+}
+
+export async function updateCardColumn(cardId:number,coluna_id:number){
+
+  await pool.query(
+    "UPDATE card SET coluna_id=$1 WHERE id=$2",
+    [coluna_id,cardId]
+  );
+
+}
